838179c9e30826cd001658b29743eca6c51e6b8c
Shihab Mahamud <shihab4t@gmail.com>
2023-03-27 04:27:01 +0600

    Youtube ui

--- building-the-youtube-ui-with-tailwind_shadee-merhi/components/SideNavigation.tsx
+++ building-the-youtube-ui-with-tailwind_shadee-merhi/components/SideNavigation.tsx
@@ -1,5 +1,34 @@
+import { IconType } from "react-icons";
+import { HiHome } from "react-icons/hi";
+import { TfiYoutube } from "react-icons/tfi";
+
+type TMenuItem = {
+    icon: IconType;
+    text: string;
+};
+
+const menuItems: TMenuItem[] = [
+    {
+        text: "Home",
+        icon: HiHome,
+    },
+    {
+        text: "Shorts",
+        icon: TfiYoutube,
+    },
+];
+
 const SideNavigation = () => {
-    return <div className=" border-2 border-orange-300">SIDE NAVIGATION</div>;
+    return (
+        <div className="border-2 border-orange-300">
+            {menuItems.map(item => (
+                <div>
+                    <item.icon />
+                    <span>{item.text}</span>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default SideNavigation;
