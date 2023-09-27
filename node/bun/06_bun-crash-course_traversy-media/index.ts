505fe1714cb06e2c35c0b614ba3bf09de7a015e7
Shihab Mahamud <shihab4t@gmail.com>
2023-09-27 21:47:15 +0600

    bun crash course

---
+++ node/bun/06_bun-crash-course_traversy-media/index.ts
@@ -0,0 +1,12 @@
+// const server = Bun.serve({
+//   port: Bun.env.PORT,
+//   fetch: (req) => {
+//     return new Response("Hello");
+//   },
+// });
+
+// console.log(`Listening on port: ${server.port}`);
+
+["a", "b", "c"].map((x) => {
+  console.log(x);
+});
