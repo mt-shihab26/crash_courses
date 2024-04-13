6ec13b948e7a906dc3a78798b007024682b5ce55
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-04-14 01:42:59 +0600

    interface

---
+++ go/topics/interface/interface1_test.go
@@ -0,0 +1,20 @@
+package interface1
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestInterface(t *testing.T) {
+	shapes := []shape{
+		square{length: 15.2},
+		circle{radius: 7.5},
+		circle{radius: 12.3},
+		square{length: 4.9},
+	}
+
+	for _, v := range shapes {
+		printShapeInfo(v)
+		fmt.Println("---")
+	}
+}
