cd043a42e9b7ffb1c26f310f7cd5498cc0fc195a
Shihab Mahamud <67628903+p-nerd@users.noreply.github.com>
2024-04-25 02:44:50 +0600

    htmx

--- javascript/27_htmx-crash-course_traversy-media/main.js
+++ javascript/27_htmx-crash-course_traversy-media/main.js
@@ -1,25 +1,28 @@
-import express from "express"
+import express from "express";
 
 const app = express();
 
 // Set static folder
 app.use(express.static("public"));
 // Parse URL-encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
-app.use(express.json())
+app.use(express.json());
 
 app.use("/users", async (req, res) => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await response.json(); 
-    return res.send(`
-        <h2 class="text-2xl font-bold my-4">Users</h2> 
-        <ul>
-            ${users.map(user =>`<li>${user.name}</li>`).join("")} 
-        </ul>
-    `)
-})
+    const limit = Number(req.query.limit || 10);
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users?_limit=${limit}`);
+    const users = await response.json();
+    setTimeout(() => {
+        res.send(`
+            <h2 class="text-2xl font-bold my-4">Users</h2> 
+            <ul>
+                ${users.map(user => `<li>${user.name}</li>`).join("")} 
+            </ul>
+        `);
+    }, 1000);
+});
 
 app.listen(8000, () => {
-    console.log("Listening on http://127.0.0.1:8000")
+    console.log("Listening on http://127.0.0.1:8000");
 });
