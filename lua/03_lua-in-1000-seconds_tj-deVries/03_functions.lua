a90e992a40e2f6bed9c9ae72833040c61099f08d
Shihab Mahamud <shihab4t@gmail.com>
2024-12-09 20:01:12 +0600

    lua functions

--- lua/03_lua-in-1000-seconds_tj-deVries/03_functions.lua
+++ lua/03_lua-in-1000-seconds_tj-deVries/03_functions.lua
@@ -10,3 +10,13 @@ local great = function(name)
 end
 
 great("Shetu")
+
+local higher_order = function(value)
+    return function(another)
+        return value + another
+    end
+end
+
+local add_one = higher_order(1)
+
+print("add_one(2) -> ", add_one(2))
