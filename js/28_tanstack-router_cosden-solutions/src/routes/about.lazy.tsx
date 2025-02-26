4ba54347c7f72f1dc89903a216b7c876b222a9e6
Shihab Mahamud <shihab4t@gmail.com>
2025-02-27 00:10:48 +0600

    remove

--- js/28_tanstack-router_cosden-solutions/src/routes/about.lazy.tsx
+++
@@ -1,9 +0,0 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
-
-const About = () => {
-    return <div>Hello "/about"!</div>;
-};
-
-export const Route = createLazyFileRoute("/about")({
-    component: About,
-});
--- js/28_tanstack-router_cosden-solutions/src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-const Index = () => {
-    return <div>Hello "/"!</div>;
-};
-
-export const Route = createFileRoute("/")({
-    component: Index,
-});
