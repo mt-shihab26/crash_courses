dfcb76a7487d7af6e3b8579f928061f9b93c8ee2
Shihab Mahamud <shihab4t@gmail.com>
2025-04-11 05:14:55 +0600

    blocks

--- ruby/01_learn-from-claude/12_blocks.rb
+++ ruby/01_learn-from-claude/12_blocks.rb
@@ -5,5 +5,19 @@ end
 puts("\n\n")
 
 # one line block
-
 5.times { |i| puts("Count #{i}") }
+
+# map for transform
+numbers = [1, 2, 3, 4, 5]
+squares = numbers.map do |number|
+  number * number
+end
+
+puts(squares)
+
+# filter with select
+even_numbers = numbers.select do |number|
+  number.even?
+end
+
+puts(even_numbers)
