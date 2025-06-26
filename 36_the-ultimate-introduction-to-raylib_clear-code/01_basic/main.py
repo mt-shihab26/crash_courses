3f82a03159cba116ef1a2604ad3e39103add252b
Shihab Mahamud <shihab4t@gmail.com>
2025-06-26 12:14:27 +0600

    basic

--- 36_the-ultimate-introduction-to-raylib_clear-code/01_basic/main.py
+++ 36_the-ultimate-introduction-to-raylib_clear-code/01_basic/main.py
@@ -1,5 +1,14 @@
+from pyray import *
+from raylib import *
+
 def main():
-    print("Hello from 01-basic!")
+    init_window(800, 450, "Basic")
+
+    while not window_should_close():
+        begin_drawing()
+        end_drawing()
+
+    close_window()
 
 
 if __name__ == "__main__":
