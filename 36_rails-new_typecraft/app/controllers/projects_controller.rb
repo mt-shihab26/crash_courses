7ed704ef69a1b808c4674d830f1fac56585cb45b
Shihab Mahamud <shihab4t@gmail.com>
2025-08-08 02:42:49 +0600

    project create

--- 36_rails-new_typecraft/app/controllers/projects_controller.rb
+++ 36_rails-new_typecraft/app/controllers/projects_controller.rb
@@ -1,11 +1,28 @@
 class ProjectsController < ApplicationController
-  # GET /projects or /projects.json
   def index
     @projects = Project.all
   end
 
-  # GET /projects/1 or /projects/1.json
   def show
     @project = Project.find(params[:id])
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render.new
+    end
+  end
+
+  private
+    def project_params
+      params.expect(project: [ :name ])
+    end
 end
--- 36_rails-new_typecraft/config/routes.rb
+++ 36_rails-new_typecraft/config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get "/projects", to: "projects#index"
+  get "/projects/new", to: "projects#new", as: "new_project"
   get "/projects/:id", to: "projects#show", as: "project"
+  post "/projects", to: "projects#create"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
