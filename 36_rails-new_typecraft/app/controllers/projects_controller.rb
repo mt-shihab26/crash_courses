b4f0d20fb7baee40f7a548435d7efae210953e0c
Shihab Mahamud <shihab4t@gmail.com>
2025-08-08 06:36:53 +0600

    delete

--- 36_rails-new_typecraft/app/controllers/projects_controller.rb
+++ 36_rails-new_typecraft/app/controllers/projects_controller.rb
@@ -15,6 +15,7 @@ class ProjectsController < ApplicationController
     @project = Project.new(project_params)
 
     if @project.save
+      flash[:notice] = "Project created successfully"
       redirect_to project_path(@project)
     else
       render :new, status: :unprocessable_entity
@@ -29,10 +30,20 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
 
     if @project.update(project_params)
+      flash[:notice] = "Project updated successfully"
       redirect_to project_path(@project)
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+
+    @project.destroy
+    flash[:notice] = "Project deleted successfully"
+
+    redirect_to projects_path
   end
 
   private
--- 36_rails-new_typecraft/config/routes.rb
+++ 36_rails-new_typecraft/config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   get "/projects", to: "projects#index"
   get "/projects/new", to: "projects#new", as: "new_project"
   get "/projects/:id", to: "projects#show", as: "project"
-  post "/projects", to: "projects#create"
+  post "/projects", to: "projects#create", as: "create_project"
   get "/projects/:id/edit", to: "projects#edit", as: "edit_project"
   patch "/projects/:id", to: "projects#update", as: "update_project"
+  delete "/projects/:id", to: "projects#destroy", as: "destroy_project"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
