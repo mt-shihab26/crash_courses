a6f4be1cdbb7881e679538d8997810aa85a51571
Shihab Mahamud <shihab4t@gmail.com>
2025-08-29 13:34:24 +0600

    concurrently

---
+++ 37_build-ai-powered-apps_programming-with-mosh/ai-app/main.ts
@@ -0,0 +1,16 @@
+import concurrently from "concurrently";
+
+concurrently([
+    {
+        name: "server",
+        cwd: "packages/server",
+        command: "bun run dev",
+        prefixColor: "cyan",
+    },
+    {
+        name: "client",
+        cwd: "packages/client",
+        command: "bun run dev",
+        prefixColor: "green",
+    },
+]);
