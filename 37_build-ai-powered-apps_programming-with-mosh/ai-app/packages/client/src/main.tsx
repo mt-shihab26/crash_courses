552b2642660129e5469b857ee301db3a3f08b37f
Shihab Mahamud <shihab4t@gmail.com>
2025-08-29 12:17:08 +0600

    client project

---
+++ 37_build-ai-powered-apps_programming-with-mosh/ai-app/packages/client/src/main.tsx
@@ -0,0 +1,13 @@
+import "./main.css";
+
+import { createRoot } from "react-dom/client";
+
+import { StrictMode } from "react";
+
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
