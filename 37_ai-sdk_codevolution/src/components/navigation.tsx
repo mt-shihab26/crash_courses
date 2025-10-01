28292067664b0ab886de4e91ab36c7edcaadd101
Shihab Mahamud <shihab4t@gmail.com>
2025-10-02 05:47:01 +0600

    navigation

--- 37_ai-sdk_codevolution/src/components/navigation.tsx
+++ 37_ai-sdk_codevolution/src/components/navigation.tsx
@@ -1,40 +1,29 @@
-import {
-    SignedIn,
-    SignedOut,
-    SignInButton,
-    SignOutButton,
-    SignUpButton,
-} from "@clerk/nextjs";
-import { Button } from "./ui/button";
-import Link from "next/link";
+import { SignedIn, SignedOut, SignInButton, SignOutButton, SignUpButton } from '@clerk/nextjs';
+import { Button } from './ui/button';
+import Link from 'next/link';
 
 export const Navigation = () => {
     return (
         <nav className="border-b border-foreground/10">
-            <div className="flex container h-16 items-center justify-between px-4 mx-auto">
-                <div className="flex items-center gap-6">
+            <div className="flex container h-16 gap-6 items-center justify-between px-4 mx-auto">
+                <div className="flex w-full items-center gap-6">
                     <Link href="/" className="text-xl font-semibold">
                         Chatbot
                     </Link>
-                    <div className="flex gap-4">
-                        <Link
-                            href="/"
-                            className="text-sm font-medium hover:text-primary transition-colors"
-                        >
-                            Home
-                        </Link>
-                        <Link
-                            href="/chat"
-                            className="text-sm font-medium hover:text-primary transition-colors"
-                        >
-                            Chat
-                        </Link>
-                        <Link
-                            href="/upload"
-                            className="text-sm font-medium hover:text-primary transition-colors"
-                        >
-                            Upload
-                        </Link>
+                    <div className="flex w-full justify-between items-center">
+                        <div className="flex gap-4 items-center">
+                            <Link href="/" className="text-sm font-medium hover:text-primary transition-colors">
+                                Home
+                            </Link>
+                            <Link href="/chat" className="text-sm font-medium hover:text-primary transition-colors">
+                                Chat
+                            </Link>
+                        </div>
+                        <div className="flex gap-4 items-center">
+                            <Link href="/upload" className="text-sm font-medium hover:text-primary transition-colors">
+                                Upload
+                            </Link>
+                        </div>
                     </div>
                 </div>
                 <div className="flex gap-2">
