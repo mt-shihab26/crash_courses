0983ec5595ba7265fa1fa5e9ec0075fad55fa2fa
Shihab Mahamud <shihab4t@gmail.com>
2025-10-01 23:16:47 +0600

    db schema

--- 37_build-a-rag-chatbot-from-scratch_codevolution/src/lib/schema.ts
+++ 37_build-a-rag-chatbot-from-scratch_codevolution/src/lib/schema.ts
@@ -1,7 +1,14 @@
-import { pgTable, serial, text, vector } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, vector, index } from 'drizzle-orm/pg-core';
 
-export const documents = pgTable('documents', {
-    id: serial('id').primaryKey(),
-    content: text('content').notNull(),
-    embedding: vector('embedding', { dimensions: 1536 }),
-});
+export const documents = pgTable(
+    'documents',
+    {
+        id: serial('id').primaryKey(),
+        content: text('content').notNull(),
+        embedding: vector('embedding', { dimensions: 1536 }),
+    },
+    table => [index('embeddingIndex').using('hnsw', table.embedding.op('vector_cosine_ops'))],
+);
+
+export type TInsertDocument = typeof documents.$inferInsert;
+export type TSelectDocument = typeof documents.$inferSelect;
