cfa06432c1aecb4e15e1edcc6d4fac8ca13375fd
Shihab Mahamud <shihab4t@gmail.com>
2025-10-02 06:05:12 +0600

    layout

---
+++ 37_ai-sdk_codevolution/src/app/upload/layout.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from 'next';
+import type { ReactNode } from 'react';
+
+export const metadata: Metadata = {
+    title: 'Upload PDF',
+};
+
+const Layout = ({ children }: Readonly<{ children: ReactNode }>) => {
+    return <>{children}</>;
+};
+
+export default Layout;
