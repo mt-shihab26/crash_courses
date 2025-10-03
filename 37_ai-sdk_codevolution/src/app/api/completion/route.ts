dd1177bd3b238a23e71b9e50f55d5a1a028885ea
Shihab Mahamud <shihab4t@gmail.com>
2025-10-03 19:36:09 +0600

    route

--- 37_ai-sdk_codevolution/src/app/api/completion/route.ts
+++ 37_ai-sdk_codevolution/src/app/api/completion/route.ts
@@ -1,24 +1,27 @@
-import { openai } from "@ai-sdk/openai";
-import { generateText } from "ai";
+import { openai } from '@ai-sdk/openai';
+import { generateText } from 'ai';
 
 export const POST = async (req: Request) => {
     try {
         const data = await req.json();
 
-        console.log(data);
-
         const prompt = data.prompt;
 
         const { text } = await generateText({
-            model: openai("gpt-4.1-nano"),
+            model: openai('gpt-4.1-nano'),
             prompt,
         });
 
         return Response.json({ text });
     } catch (e: any) {
-        console.error("Error:", e);
-        return Response.json({
-            error: e instanceof Error ? e.message : "Something went wrong",
-        });
+        console.error('Error:', e);
+        return Response.json(
+            {
+                error: e instanceof Error ? e.message : 'Something went wrong',
+            },
+            {
+                status: 500,
+            },
+        );
     }
 };
