8a37c4b5824f7fb2b93578ccb31c60ece1682071
Shihab Mahamud <shihab4t@gmail.com>
2025-10-07 01:23:44 +0600

    tools

---
+++ 37_ai-sdk_codevolution/src/app/api/tools/route.ts
@@ -0,0 +1,54 @@
+import type { InferUITools, UIDataTypes, UIMessage } from "ai";
+
+import { openai } from "@ai-sdk/openai";
+import { convertToModelMessages, streamText, tool } from "ai";
+import { z } from "zod";
+
+const tools = {
+    getWeather: tool({
+        description: "Get the weather for a location",
+        inputSchema: z.object({
+            city: z.string().describe("The city to get the weather for"),
+        }),
+        execute: async ({ city }) => {
+            if (city.toLowerCase().includes("tokiyo")) {
+                return "70F and cloudy";
+            }
+            if (city.toLowerCase().includes("dhaka")) {
+                return "80F and sunny";
+            }
+            return `Unknown`;
+        },
+    }),
+};
+
+export type TTools = InferUITools<typeof tools>;
+export type TMessage = UIMessage<never, UIDataTypes, TTools>;
+
+export const POST = async (req: Request) => {
+    try {
+        const { messages }: { messages: TMessage[] } = await req.json();
+
+        const result = streamText({
+            model: openai("gpt-4.1-nano"),
+            messages: convertToModelMessages(messages),
+            tools,
+        });
+
+        result.usage.then(usage => {
+            console.log({
+                messagesCount: messages.length,
+                inputTokens: usage.inputTokens,
+                outputTokens: usage.outputTokens,
+                totalTokens: usage.totalTokens,
+            });
+        });
+
+        return result.toUIMessageStreamResponse();
+    } catch (e: any) {
+        console.error("Error chat:", e);
+        return new Response(e instanceof Error ? e.message : "Something went wrong", {
+            status: 500,
+        });
+    }
+};
---
+++ 37_ai-sdk_codevolution/src/app/tools/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { useChat } from "@ai-sdk/react";
+import { useState } from "react";
+
+import { DefaultChatTransport } from "ai";
+
+const ChatPage = () => {
+    const [prompt, setPrompt] = useState<string>("");
+
+    const { messages, error, sendMessage, status, stop } = useChat({
+        transport: new DefaultChatTransport({ api: "/api/tools" }),
+    });
+
+    const submit = async () => {
+        sendMessage({ text: prompt });
+        setPrompt("");
+    };
+
+    return (
+        <div className="flex flex-col w-full max-w-md mx-auto h-[calc(100vh-6rem)]">
+            <div className="flex-1 overflow-y-auto py-4 px-4 pb-20">
+                {messages.map(message => (
+                    <div key={message.id} className="mb-4">
+                        <div className="font-semibold">
+                            {message.role === "user" ? "You" : "AI"}:{" "}
+                        </div>
+                        {message.parts.map((part, index) => {
+                            switch (part.type) {
+                                case "text":
+                                    return (
+                                        <div
+                                            key={`${message.id}-${index}`}
+                                            className="whitespace-pre-wrap"
+                                        >
+                                            {part.text}
+                                        </div>
+                                    );
+                                default:
+                                    return null;
+                            }
+                        })}
+                    </div>
+                ))}
+                {error && <div className="text-red-500 mb-4">{error.message}</div>}
+                {status !== "ready" && <div>Loading...</div>}
+            </div>
+            <form
+                className="fixed bottom-0 w-full max-w-md mx-auto left-0 right-0 p-4 bg-zinc-50"
+                onSubmit={e => {
+                    e.preventDefault();
+                    submit();
+                }}
+            >
+                <div className="flex gap-2">
+                    <input
+                        placeholder="How can I help you?"
+                        className="flex-1 p-2 border border-zinc-300"
+                        value={prompt}
+                        onChange={e => setPrompt(e.target.value)}
+                    />
+                    {status !== "ready" ? (
+                        <button
+                            type="button"
+                            onClick={stop}
+                            className="bg-red-500 text-white px-4 py-2 rounded transition"
+                        >
+                            Stop
+                        </button>
+                    ) : (
+                        <button
+                            disabled={status !== "ready"}
+                            type="submit"
+                            className="bg-blue-500 text-white px-4 py-2 rounded transition"
+                        >
+                            Send
+                        </button>
+                    )}
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default ChatPage;
