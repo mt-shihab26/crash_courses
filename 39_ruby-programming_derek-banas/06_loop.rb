b68ee3024623e397b0058622049a943fa3396f76
Shihab Mahamud <shihab4t@gmail.com>
2026-01-07 18:55:35 +0600

    loop

--- 39_ruby-programming_derek-banas/06_loop.rb
+++ 39_ruby-programming_derek-banas/06_loop.rb
@@ -9,3 +9,17 @@ loop do
 
   break if x >= 10
 end
+
+numbers = [1, 2, 3, 4, 5]
+
+for number in numbers
+  puts "#{number}"
+end
+
+numbers.each do |number|
+  puts "#{number}"
+end
+
+(0...5).each do |number|
+  puts "#{number}"
+end
