960e3109606ebedfbc13c2320400b354f7cb96e5
Shihab Mahamud <shihab4t@gmail.com>
2026-01-08 02:55:29 +0600

    product model

--- 40_rails-store_guides.rubyonrails.org/db/schema.rb
+++ 40_rails-store_guides.rubyonrails.org/db/schema.rb
@@ -10,5 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_07_205140) do
+  create_table "products", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
 end
