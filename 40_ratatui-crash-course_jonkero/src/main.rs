a87d957e31138c6334137c8d66c92cc1258bfb1b
Shihab Mahamud <shihab4t@gmail.com>
2026-03-13 14:11:11 +0600

    basic app

--- 40_ratatui-crash-course_jonkero/src/main.rs
+++ 40_ratatui-crash-course_jonkero/src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Result;
+
+use ratatui::{
+    DefaultTerminal, Frame,
+    crossterm::event::{self, Event, KeyCode, KeyEventKind},
+};
+
+fn main() -> Result<()> {
+    let mut terminal = ratatui::init();
+
+    let result = run(&mut terminal);
+
+    ratatui::restore();
+
+    result
+}
+
+fn run(terminal: &mut DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if handle_events()? {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    //
+}
+
+fn handle_events() -> Result<bool> {
+    match event::read()? {
+        Event::Key(key) => {
+            if key.kind == KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Char('q') => return Ok(true)
+                    _ => {}
+                }
+            }
+        }
+        _ => {}
+    }
+
+    Ok(false)
 }
