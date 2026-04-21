a5bf9a4a4770396827a0139e6f7cfcba3f46818b
Shihab Mahamud <shihab4t@gmail.com>
2026-04-21 09:57:22 +0600

    cors

--- 42_build-web-scraper-in-go_boot.dev/go.mod
+++ 42_build-web-scraper-in-go_boot.dev/go.mod
@@ -3,3 +3,5 @@ module ws
 go 1.26.2
 
 require github.com/go-chi/chi/v5 v5.2.5
+
+require github.com/go-chi/cors v1.2.2 // indirect
--- 42_build-web-scraper-in-go_boot.dev/main.go
+++ 42_build-web-scraper-in-go_boot.dev/main.go
@@ -7,21 +7,25 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func main() {
 	router := chi.NewRouter()
-
 	router.Use(middleware.Logger)
-
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
-
 	port := "3000"
-
 	fmt.Printf("Server is running on http://localhost:%s\n", port)
-
 	err := http.ListenAndServe(fmt.Sprintf(":%s", port), router)
 	if err != nil {
 		log.Fatal(err)
