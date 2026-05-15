34bb4ed86ab5991ec90fc3cd0e1207585f87ff86
Shihab Mahamud <shihab4t@gmail.com>
2026-05-15 07:23:12 +0600

    fetch todos

--- 43_learn-tanstack-start_web-dev-simplified/src/db/schema.ts
+++ 43_learn-tanstack-start_web-dev-simplified/src/db/schema.ts
@@ -4,6 +4,7 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 export const todos = sqliteTable('todos', {
     id: integer({ mode: 'number' }).primaryKey({ autoIncrement: true }),
     title: text().notNull(),
+    completedAt: integer({ mode: 'timestamp' }),
     createdAt: integer({ mode: 'timestamp' }).default(sql`(unixepoch())`),
     updatedAt: integer({ mode: 'timestamp' }).default(sql`(unixepoch())`),
 });
