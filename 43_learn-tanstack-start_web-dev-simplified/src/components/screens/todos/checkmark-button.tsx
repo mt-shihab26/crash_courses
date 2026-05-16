ba73c4ed55a0ac9963d5728166e160fc45e67bb8
Shihab Mahamud <shihab4t@gmail.com>
2026-05-16 07:06:51 +0600

    checkmark button

--- 43_learn-tanstack-start_web-dev-simplified/src/components/screens/todos/checkmark-button.tsx
+++ 43_learn-tanstack-start_web-dev-simplified/src/components/screens/todos/checkmark-button.tsx
@@ -1,21 +1,24 @@
 import type { TTodo } from '#/db/schema';
 
 import { toggleComplete } from '#/actions/todos';
+import { useNavigate } from '@tanstack/react-router';
 import { useState } from 'react';
 
 import { Checkbox } from '#/components/ui/checkbox';
 
 export const CheckmarkButton = ({ todo }: { todo: TTodo }) => {
+    const navigate = useNavigate();
+
     const [loading, setLoading] = useState(false);
 
     return (
         <Checkbox
-            checked={todo.completedAt != null}
-            disabled={loading}
-            onClick={() => {
+            checked={loading || todo.completedAt != null}
+            onClick={async () => {
                 try {
                     setLoading(true);
-                    toggleComplete({ data: todo.id });
+                    await toggleComplete({ data: todo.id });
+                    navigate({ to: '/todos' });
                 } finally {
                     setLoading(false);
                 }
